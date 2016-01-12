6191dfa305e6928e24527f62e7ae3a93247347bc
Anthony Tsang <anthony.tsang@soldocoin.com>
2016-01-13 02:07:47 +0800

    finish desktop version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+    'use strict';
+
+    var banner = '.subscribe .banner';
+
+    $('#to-subscribe').click(function(){
+        scrollTo(subscribeHeight(), function(){
+          $('#subscribe-form input').focus()
+        });
+    });
+    $('.scroll-down').click(function(){
+      scrollTo($('.how-it-works:eq(0)').offset().top);
+    });
+
+    function curry (func) {
+        return function (item) {
+            return $(item)[func];
+        };
+    }
+
+    function scrollTo(height, cb) {
+        $('html, body').animate({scrollTop: height}, 400, "swing", cb);
+    }
+
+    function subscribeHeight () {
+        return $(banner).offset().top - ($(window).height() - $(banner).outerHeight()) / 2;
+    }
+});
